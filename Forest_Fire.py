fa7a69c96413ea552f6df68f0d3caf42c859af32
julian801 <juliandchan@gmail.com>
2018-11-29 22:12:26 +0100

    Added py files and visualization

---
+++ Forest_Fire.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 14:38:36 2018
+
+@author: julianchan
+"""
+
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import math
+from mesa import Model, Agent
+from mesa.time import RandomActivation
+from mesa.space import Grid
+from mesa.datacollection import DataCollector
+from mesa.batchrunner import BatchRunner
+
+
+class TreeCell(Agent):
+    '''
+    A tree cell.
+    
+    Attributes:
+        x, y: Grid coordinates
+        condition: Can be "Fine", "On Fire", or "Burned Out"
+        unique_id: (x,y) tuple. 
+    
+    unique_id isn't strictly necessary here, but it's good practice to give one to each
+    agent anyway.
+    '''
+    def __init__(self, model, pos):
+        '''
+        Create a new tree.
+        Args:
+            pos: The tree's coordinates on the grid. Used as the unique_id
+        '''
+        super().__init__(pos, model)
+        self.pos = pos
+        self.unique_id = pos
+        self.condition = "Fine"
+        
+    def step(self):
+        '''
+        If the tree is on fire, spread it to fine trees nearby.
+        '''
+        if self.condition == "On Fire":
+            neighbors = self.model.grid.get_neighbors(self.pos, moore=False)
+            for neighbor in neighbors:
+                if neighbor.condition == "Fine":
+                    neighbor.condition = "On Fire"
+            self.condition = "Burned Out"
+
+##End of class
+
+
+class ForestFire(Model):
+    '''
+    Simple Forest Fire model.
+    '''
+    import random
+    def __init__(self, height, width, density):
+        '''
+        Create a new forest fire model.
+        
+        Args:
+            height, width: The size of the grid to model
+            density: What fraction of grid cells have a tree in them.
+        '''
+        self.MatrixHistory = []
+        self.matrix = np.zeros((width, height))
+        # Initialize model parameters
+        self.height = height
+        self.width = width
+        self.density = density
+        
+        # Set up model objects
+        self.schedule = RandomActivation(self)
+        self.grid = Grid(height, width, torus=False)
+        self.dc = DataCollector({"Fine": lambda m: self.count_type(m, "Fine"),
+                                "On Fire": lambda m: self.count_type(m, "On Fire"),
+                                "Burned Out": lambda m: self.count_type(m, "Burned Out")})
+        
+        # Place a tree in each cell with Prob = density
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.random.random() < self.density:
+                    # Create a tree
+                    new_tree = TreeCell(self, (x, y))
+                    # Set all trees in the first column on fire.
+                    ##Edit below to change the location of the fire <--------------------------
+                    if x == 0:
+                        new_tree.condition = "On Fire"
+                    self.grid[y][x] = new_tree
+                    ##Edits below ----------------------------------
+                    if new_tree.condition == "On Fire":
+                        self.matrix[x][y] = 1
+                    if new_tree.condition == "Burned Out":
+                        self.matrix[x][y] = 1
+                    if new_tree.condition == "Fine":
+                        self.matrix[x][y] = 0
+                    self.schedule.add(new_tree)
+        self.running = True
+        #print(self.matrix)
+
+    def step(self):
+        '''
+        Advance the model by one step.
+        '''
+        self.schedule.step()
+        self.dc.collect(self)
+        # Halt if no more fire
+        if self.count_type(self, "On Fire") == 0:
+            self.running = False
+    
+    @staticmethod
+    def count_type(model, tree_condition):
+        '''
+        Helper method to count trees in a given condition in a given model.
+        '''
+        count = 0
+        for tree in model.schedule.agents:
+            model.MatrixHistory.append(model.matrix)
+            if tree.condition == tree_condition:
+                count += 1
+            if tree.condition == "Burned Out":
+                model.matrix[tree.pos[0]][tree.pos[1]] = 1
+            if tree.condition == "On Fire":
+                model.matrix[tree.pos[0]][tree.pos[1]] = 1
+        return count
+##End of class    
+
+#fire = ForestFire(4+1, 20+1, 0.5)
+#fire.run_model()
